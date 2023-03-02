f882a487c70ac1f87ea6fb39fa8b97c535f81107
Crowdbotics <engineering@crowdbotics.com>
2023-03-02 00:15:09 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "throbbing-forest-de-67314",
+  url: "https://throbbing-forest-de-67314.botics.co",
+  api: "https://throbbing-forest-de-67314.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
